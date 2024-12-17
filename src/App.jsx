4ec46e69648fc88b92efec5bbbec34c6149d3aa0
CodrutaRisco <codruta.risco@gmail.com>
2024-12-17 11:43:22 +0000

    get articles,add header.css,add function footer,refactor App.jsx,add footer in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/header/Header";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getArticles } from "./utils/api";
+import Footer from "./components/footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -17,7 +19,22 @@ function App() {
 
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <main>
+        <div className="articles-container">
+          {items.map((article) => (
+            <div key={article.article_id} className="article-card">
+              <h2>{article.title}</h2>
+              <p>By: {article.author}</p>
+              <p>Topic: {article.topic}</p>
+              <p>
+                Published: {new Date(article.created_at).toLocaleDateString()}
+              </p>
+            </div>
+          ))}
+        </div>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
