e2f2a43f8fa3f67e0ced8b21341d23339f9a088b
CodrutaRisco <codruta.risco@gmail.com>
2025-03-03 16:01:52 +0000

    add function capitalizeFirstLetter, refactor ArticleList

--- src/components/articles-list/articles-list.jsx
+++ src/components/articles-list/articles-list.jsx
@@ -1,138 +1,77 @@
 import { useState, useEffect } from "react";
-import { getArticles } from "../../utils/api";
-import ArticleCard from "../article-card/article-card";
+import { Link } from "react-router";
+import { getArticles, getArticleByTopic } from "../../utils/api";
 import "./articles-list.css";
-import { useSearchParams } from "react-router";
-import { SearchByTopic } from "../topics/SearchByTopic";
-
+import Votes from "../votes/votes";
+import capitalizeFirstLetter from "../../utils/capitalize-first-letter";
 
 // eslint-disable-next-line react/prop-types
-function ArticlesList({ loadingLottie }) {
+function ArticlesList({ topic }) {
   const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const topic = searchParams.get("topic") || "";
-  const sort_by = searchParams.get("sort_by") || "created_at";
-  const order = searchParams.get("order") || "DESC";
-  const [isError, setIsError] = useState(null);
 
   useEffect(() => {
-    console.log("Fetching articles with sort_by:", sort_by, order);
-    setIsLoading(true);
-    setIsError(null);
-
-    getArticles(topic, sort_by, order)
-      .then((articlesFromApi) => {
-        // console.log("Received articles:", articlesFromApi);
-        setArticles(Array.isArray(articlesFromApi) ? articlesFromApi : []);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsError("Failed to load articles: " + error.message);
-        setIsLoading(false);
+    if (!topic) {
+      getArticles().then((articlesFromApi) => {
+        setArticles(articlesFromApi);
       });
-  }, [topic, sort_by, order]);
-
-  if (isLoading) {
-    return (
-      <>
-        <p>articles loading...</p>
-        {loadingLottie}
-      </>
+    } else {
+      getArticleByTopic(topic).then((articlesFromApi) => {
+        setArticles(articlesFromApi);
+      });
+    }
+  }, [topic]);
+  const handleVoteChange = (article_id, inc_votes) => {
+    setArticles((prevArticles) =>
+      prevArticles.map((article) =>
+        article.article_id === article_id
+          ? { ...article, votes: article.votes + inc_votes }
+          : article
+      )
     );
-  }
-  const sortedArticles = [...articles];
-
-  if (sort_by === "comment_count") {
-    sortedArticles.sort((a, b) => b.comment_count - a.comment_count);
-  }
-
-  const handleBackButton = () => {
-    setSearchParams({});
-    setIsError(null);
-  };
-  if (isError) {
-    return (
-      <>
-        <p>{isError}</p>
-        <p> Try searching another topic... </p>
-        <button onClick={handleBackButton}> Back to articles </button>
-      </>
-    );
-  }
-  const handleSortByChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setSearchParams((prev) => {
-      const params = new URLSearchParams(prev); // Ensure it's iterable
-      params.set(name, value);
-      return params;
-    });
-  };
-
-  const handleOrderChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setSearchParams((prev) => {
-      const params = new URLSearchParams(prev); // Ensure it's iterable
-      params.set(name, value);
-      return params;
-    });
   };
 
   return (
-    <>
-      <h1>
-        {" "}
-        {topic.length === 0
-          ? "Looking at all articles"
-          : `Looking at articles on ${topic}`}
-      </h1>
-      <SearchByTopic setSearchParams={setSearchParams} />
-      <select
-        id="sortBy-dropdown"
-        onChange={handleSortByChange}
-        name="sort_by"
-        value={sort_by}
-      >
-        <option disabled>Sort By</option>
-        <option value="created_at">Date</option>
-        <option value="comment_count">Comment Count</option>
-        <option value="votes">Votes</option>
-      </select>
-      <select
-        id="order-dropdown"
-        onChange={handleOrderChange}
-        name="order"
-        value={order}
-      >
-        <option disabled>Order</option>
-        <option value="DESC"> Desc</option>
-        <option value="ASC"> Asc</option>
-      </select>
-      {topic && (
-        <button onClick={handleBackButton}>Back to all articles</button>
-      )}
+    <div className="articles-container">
+      <h1>{topic ? capitalizeFirstLetter(topic) : "All News"}</h1>
+      <ul className="articles-list">
+        {articles.map((article) => (
+          <li key={article.article_id} className="article-card">
+            <div className="article-content">
+              <div className="article-image">
+                <Link to={`/articles/${article.article_id}`}>
+                  <img src={article.article_img_url} alt="article" />
+                </Link>
+              </div>
 
-      <div className="list">
-        {sortedArticles.length > 0 ? (
-          sortedArticles.map((article) => (
-            <ArticleCard
-              key={article.article_id}
-              article={article}
-              id={article.article_id}
-              image={article.article_img_url}
-              author={article.author}
-              date={article.created_at}
-              title={article.title}
-              topic={article.topic}
-            />
-          ))
-        ) : (
-          <p>No articles found for this topic.</p>
-        )}
-      </div>
-    </>
+              <div className="article-details">
+                <Link to={`/articles/${article.article_id}`}>
+                  <h2>{article.title}</h2>
+                </Link>
+                <p className="article-tag">{`topic: ${article.topic}`}</p>
+                <div className="article-meta">
+                  <span>
+                    <strong>Author:</strong> {article.author} —{" "}
+                    {article.created_at.substring(0, 10)}
+                  </span>
+                </div>
+                <div className="article-actions">
+                  <Link to={`/articles/${article.article_id}/comments`}>
+                    <button className="comment-button">{`Comments: ${article.comment_count}`}</button>
+                  </Link>
+                  <Votes
+                    vote={article.votes}
+                    article_id={article.article_id}
+                    onVoteChange={(inc_votes) =>
+                      handleVoteChange(article.article_id, inc_votes)
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
